d78417947636530a1ac6956ac218c36fc64fd16b
Noah Carlson <Noahrk14@gmail.com>
2023-10-09 16:39:26 -0600

    Make the website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import logo from './logo.svg';
+import MenuSelection from './components/MenuSelection';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="header-container">
+        <h3 className='clean-header'>Noah Carlson</h3>
+        <h5 className='clean-header opacity-50'>Software Engineer</h5>
+      </div>
+      <div className="screen-container">
+        <div className="column-container">
+          <div className="column column-left">
+            <div className="menu-container">
+              <MenuSelection icon="user" title="ABOUT" />
+              <MenuSelection icon="academics" title="ACADEMICS" />
+              <MenuSelection icon="projects" title="PROJECTS" />
+              <MenuSelection icon="resume" title="RESUME" />
+            </div>
+          </div>
+          <div className="column column-right">
+            <h3 className="left clean-header display-header">ABOUT</h3>
+            <div className='text-container'>
+              <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit ani </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+{/* <header className="App-header">
+      
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +48,5 @@ function App() {
         >
           NOOOO
         </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+        
+      </header> */}
---
+++ src/components/MenuSelection.js
@@ -0,0 +1,47 @@
+import '../stylesheets/MenuSelection.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faGraduationCap, faHammer, faBriefcase } from '@fortawesome/free-solid-svg-icons';
+
+import React from 'react';
+
+
+function MenuSelection(props) {
+    const {icon, title} = props;
+
+    const getIcon = () => {
+        if(icon === "user"){
+            return (
+                <FontAwesomeIcon icon={faUser} className='icon'/>
+            );
+        }
+        else if(icon === "academics") {
+            return (
+                <FontAwesomeIcon icon={faGraduationCap} className='icon'/>
+            );
+        }
+        else if(icon === "projects") {
+            return (
+                <FontAwesomeIcon icon={faHammer} className='icon'/>
+            );
+        }
+        else if(icon === "resume") {
+            return (
+                <FontAwesomeIcon icon={faBriefcase} className='icon'/>
+            );
+        }
+    }
+
+    return (
+        
+        <div className="menu_selection">
+            <div className="square-2"></div>
+            <div className="square-1"></div>
+
+            {getIcon()}
+            <h3 className='clean-header text'>{title}</h3>
+        </div>
+    );
+}
+
+export default MenuSelection;
+
