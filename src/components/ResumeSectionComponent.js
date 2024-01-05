60a1953e5b1e6c097b062774470dcf69adbb97e0
Noah Carlson <Noahrk14@gmail.com>
2024-01-05 12:49:03 -1000

    Update Download

--- src/components/ResumeSectionComponent.js
+++ src/components/ResumeSectionComponent.js
@@ -1,7 +1,7 @@
 function ResumeSection() {
     const handleDownload = () => {
         // Replace 'your-file-path' with the actual path to your file
-        const filePath = process.env.PUBLIC_URL + '/Noah_Resume.pdf';
+        const filePath = process.env.PUBLIC_URL + '/Noah_Carlson_Resume.pdf';
         const link = document.createElement('a');
         link.href = filePath;
         link.download = 'Noah_Carlson_Resume.pdf'; // Change the name of the downloaded file if needed
@@ -13,7 +13,7 @@ function ResumeSection() {
             <h2 className="left clean-header display-header">{"RESUME"}</h2>
             <hr className="header-hr"/>
             {/* <h3 className="display-subheader">Download</h3> */}
-            <p>Please feel free to download my resume here!</p>
+            <p>Please feel free to download my resume!</p>
             <button className="download-button" onClick={handleDownload}>Download</button>
             {/* <a href="https://docs.google.com/document/d/1H6hLrKS-BRqscl6EAaEpt3FgsIQwbZO9VQ5Hw3MguEA/edit?usp=sharing">View Resume</a> */}
             <p>Or view it right here - </p>
